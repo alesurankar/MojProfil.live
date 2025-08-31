27fbf8e154087ee5c02ef57dce67c0508dda9038
ales2026 <ale.urankar@gmail.com>
2025-08-31 20:13:05 +0200

    adding skill description for HTML and CSS, with box animation

--- Skills/skills.js
+++ Skills/skills.js
@@ -2,5 +2,13 @@ document.getElementById("content").addEventListener("click", (e) => {
   if (e.target.classList.contains("skill-btn")) {
     const details = e.target.nextElementSibling;
     details.classList.toggle("active");
+
+    if (details.classList.contains("active")) {
+      // expand to full scrollHeight
+      details.style.maxHeight = details.scrollHeight + "px";
+    } else {
+      // collapse back
+      details.style.maxHeight = "0";
+    }
   }
 });
